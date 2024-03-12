2b397764483ed1277c67a25578692170def813ff
thinhnguyen <thinhnq@blockchainlabs.asia>
2024-03-12 11:27:14 +0000

    green on chain

--- nft/onchain/NFT/OnChain/NFT/Compiled.hs
+++ nft/onchain/NFT/OnChain/NFT/Compiled.hs
@@ -5,15 +5,16 @@ module NFT.OnChain.NFT.Compiled
     ( nftPolicy
     ) where
 
+import PlutusLedgerApi.V3 ( TxId, BuiltinData )
 import qualified PlutusTx
-
-import           NFT.OnChain.NFT (mkWrappedNFTPolicy)
-import           PlutusLedgerApi.V1         (TokenName)  
-import           PlutusLedgerApi.V2       (BuiltinData)
-import           PlutusCore.Version      (plcVersion100)
+import PlutusLedgerApi.V2 (TokenName)
+import PlutusCore.Version (plcVersion100)
+import NFT.OnChain.NFT (mkWrappedNFTPolicy)
 
 -- | Generates validator given parameter.
-nftPolicy ::  TokenName -> PlutusTx.CompiledCode (BuiltinData -> BuiltinData -> ())
-nftPolicy tn = 
-    $$(PlutusTx.compile [|| mkWrappedNFTPolicy||]) 
+nftPolicy ::  TxId -> Integer -> TokenName -> PlutusTx.CompiledCode (BuiltinData -> BuiltinData -> ())
+nftPolicy txid ix tn =
+    $$(PlutusTx.compile [|| mkWrappedNFTPolicy||])
+        `PlutusTx.unsafeApplyCode` PlutusTx.liftCode plcVersion100 txid
+        `PlutusTx.unsafeApplyCode` PlutusTx.liftCode plcVersion100 ix
         `PlutusTx.unsafeApplyCode` PlutusTx.liftCode plcVersion100 tn
