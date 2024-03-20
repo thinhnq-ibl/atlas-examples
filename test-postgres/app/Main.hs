e5f8ac06ccdfacf8466434cf9555379cd4da406d
thinhnguyen <thinhnq@blockchainlabs.asia>
2024-03-20 11:11:59 +0000

    temp

--- test-postgres/app/Main.hs
+++ test-postgres/app/Main.hs
@@ -4,11 +4,13 @@ module Main (main) where
 import           Database.PostgreSQL.Simple
 import           Lib
 import           Text.Printf                (printf)
+import qualified Data.Text as Text
 
 main :: IO ()
 main = do
     someFunc
-    let conString = "postgresql://postgres:changeme@192.168.1.3:5432/postgres";
+    let conString = "postgresql://postgres:changeme@192.168.1.3:5432/oracle";
     conn <- connectPostgreSQL conString
-    [Only i] <- query_ conn "select 2 + 2"
-    printf i
+    xs <- query_ conn "select id, usd, vnd from price"
+    forM_ xs $ \(id, usd, vnd) ->
+        putStrLn $ Text.unpack usd ++ " is " ++ show (vnd :: Int)
