7ad25345ed7a69d1e45b3bb5e474de941df6a858
thinhnguyen <thinhnq@blockchainlabs.asia>
2024-03-21 04:00:35 +0000

    show Object

--- test-postgres/app/Main.hs
+++ test-postgres/app/Main.hs
@@ -1,17 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
 module Main (main) where
 
 import           Database.PostgreSQL.Simple
 import           Lib
 import Control.Monad
-import Data.Int
+import GHC.Generics
+
+data Oracle = Oracle {
+    id :: Int,
+    usd :: Double,
+    vnd :: Int
+    }
+  deriving (Show, Generic)           -- Derive Generic instance,
+  deriving anyclass (ToRow, FromRow) -- used for these instances
 
 main :: IO ()
 main = do
     someFunc
     let conString = "postgresql://postgres:changeme@192.168.1.3:5432/oracle";
     conn <- connectPostgreSQL conString
-    xs :: [(Int64, Double, Int64)] <- query_ conn "select id, usd, vnd from price"
-    forM_ xs $ \(id, usd, vnd) ->
-        putStrLn $ show id ++ " " ++ show usd ++ " " ++ show vnd 
+    xs :: [Oracle] <- query_ conn "select id, usd, vnd from price"
+    forM_ xs $ \x ->
+        print x
