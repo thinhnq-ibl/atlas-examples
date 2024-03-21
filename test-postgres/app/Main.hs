54dc7a6ea8337eee66aa8ee3d31a0fa03b17453b
thinhnguyen <thinhnq@blockchainlabs.asia>
2024-03-21 03:18:21 +0000

    can query

--- test-postgres/app/Main.hs
+++ test-postgres/app/Main.hs
@@ -1,16 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main (main) where
 
 import           Database.PostgreSQL.Simple
 import           Lib
-import           Text.Printf                (printf)
-import qualified Data.Text as Text
+import Control.Monad
+import Data.Int
 
 main :: IO ()
 main = do
     someFunc
     let conString = "postgresql://postgres:changeme@192.168.1.3:5432/oracle";
     conn <- connectPostgreSQL conString
-    xs <- query_ conn "select id, usd, vnd from price"
+    xs :: [(Int64, Double, Int64)] <- query_ conn "select id, usd, vnd from price"
     forM_ xs $ \(id, usd, vnd) ->
-        putStrLn $ Text.unpack usd ++ " is " ++ show (vnd :: Int)
+        putStrLn $ show id ++ " " ++ show usd ++ " " ++ show vnd 
